b01bfb40e19fcc0b765434296c38c3cb923681b7
skeletus-design <nikita.voro24nin@gmail.com>
2022-11-29 00:14:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,34 @@ const student = "Воронин Никита Валерьевич";
 
 document.getElementById("student").innerHTML = student;
 
-let a = prompt('предполагаемый результат первой команды')
+let a = prompt('предполагаемый результат первой команды') 
 let b = prompt('предполагаемый результат второй команды') 
-let c = prompt('реальный результат первой команды')
-let d = prompt('реальный результат второй команды')   
+let c = prompt('реальный результат первой команды') 
+let d = prompt('реальный результат второй команды') 
 let score_real = [c,d];
 let score_expected = [c,d];
-if  
-(a>b) {score_real = 0} 
-else if 
-(a==b) {score_real = 1}
+if (a>b) {score_real = 0
+} 
+else if (a==b) {
+  score_real = 1
+}
 else
- score_real = 2
-if  
-(c>d) {score_expected = 0} 
-else if
- (c==d) {score_expected = 1}
+  score_real = 2
+if (c>d) {
+  score_expected = 0
+} 
+else if (c==d) {
+  score_expected = 1
+}
 else 
-score_expected = 2 
+  score_expected = 2 
  if (b==d,a==c) {
-    alert( 'Счёт и результат были угаданы верно !' );
+   alert( 'Счёт и результат были угаданы верно !' );
    }  
-else if  (score_real == score_expected) {
-  alert( 'Счёт не угадали!' )
-    alert( 'Результат был угадан верно!' );
+else if (score_real == score_expected) {
+   alert( 'Счёт не угадали, но исход угадали' );
 }
 else
 { 
-  alert( 'Счёт не угадали, результат не был угадан!' );
+   alert( 'Счёт не угадали, результат не был угадан!' );
 }
